72e7195a883ad6944b26db6dde03fbb9cbfb68c4
Luis <lfcaicedo1987@gmail.com>
2023-11-14 00:00:48 -0500

    Update imagen openvas

--- proyec-vulnerabilities/openvas-docker/openvas-docker.sh
+++ proyec-vulnerabilities/openvas-docker/openvas-docker.sh
@@ -1,1 +1,1 @@
-docker run --rm --name openvas-vulnerabilities -p 8082:80 -d luisfercho09/openvas-vulnerabilities
+docker run -it --name openvas-vulnerabilities -p 8082:80 -d luisfercho09/openvas-vulnerabilities
